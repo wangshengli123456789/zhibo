e238f39ecab972078e946076902b5422214a0029
王胜利 <1414210968@qq.com>
2018-12-06 17:44:03 +0800

    修改

--- resources/views/lyout/master.blade.php
+++ resources/views/lyout/master.blade.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>『有主机上线』后台管理</title>
+        <title>后台管理</title>
         <link rel="stylesheet" type="text/css" href="{{asset('login')}}/css/common.css"/>
         <link rel="stylesheet" type="text/css" href="{{asset('login')}}/css/main.css"/>
         <script type="text/javascript" src="{{asset('login')}}/js/libs/modernizr.min.js"></script>
@@ -10,16 +10,15 @@
     <div class="topbar-wrap white">
         <div class="topbar-inner clearfix">
             <div class="topbar-logo-wrap clearfix">
-                <h1 class="topbar-logo none"><a href="index.html" class="navbar-brand">后台管理</a></h1>
                 <ul class="navbar-list clearfix">
-                    <li><a class="on" href="index.html">首页</a></li>
+                    <li><a class="on" href="/index">首页</a></li>
                 </ul>
             </div>
             <div class="top-info-wrap">
                 <ul class="top-info-list clearfix">
                     <li><a href="http://www.jscss.me">管理员</a></li>
                     <li><a href="http://www.jscss.me">修改密码</a></li>
-                    <li><a href="http://www.jscss.me">退出</a></li>
+                    <li><a href="/exit">退出</a></li>
                 </ul>
             </div>
         </div>
@@ -37,19 +36,23 @@
                             <li><a href="design.html"><i class="icon-font">&#xe008;</i>作品管理</a></li>
                             <li><a href="design.html"><i class="icon-font">&#xe005;</i>博文管理</a></li>
                             <li><a href="design.html"><i class="icon-font">&#xe006;</i>分类管理</a></li>
-                            <li><a href="design.html"><i class="icon-font">&#xe004;</i>留言管理</a></li>
-                            <li><a href="design.html"><i class="icon-font">&#xe012;</i>评论管理</a></li>
-                            <li><a href="design.html"><i class="icon-font">&#xe052;</i>友情链接</a></li>
-                            <li><a href="design.html"><i class="icon-font">&#xe033;</i>广告管理</a></li>
                         </ul>
                     </li>
                     <li>
                         <a href="#"><i class="icon-font">&#xe018;</i>系统管理</a>
                         <ul class="sub-menu">
                             <li><a href="system.html"><i class="icon-font">&#xe017;</i>系统设置</a></li>
-                            <li><a href="system.html"><i class="icon-font">&#xe037;</i>清理缓存</a></li>
-                            <li><a href="system.html"><i class="icon-font">&#xe046;</i>数据备份</a></li>
-                            <li><a href="system.html"><i class="icon-font">&#xe045;</i>数据还原</a></li>
+                            {{--<li><a href="system.html"><i class="icon-font">&#xe037;</i>清理缓存</a></li>--}}
+                            {{--<li><a href="system.html"><i class="icon-font">&#xe046;</i>数据备份</a></li>--}}
+                            {{--<li><a href="system.html"><i class="icon-font">&#xe045;</i>数据还原</a></li>--}}
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#"><i class="icon-font">&#xe018;</i>权限管理</a>
+                        <ul class="sub-menu">
+                            <li><a href="system.html"><i class="icon-font">&#xe017;</i>用户管理</a></li>
+                            <li><a href="system.html"><i class="icon-font">&#xe037;</i>角色管理</a></li>
+                            <li><a href="system.html"><i class="icon-font">&#xe046;</i>权限管理</a></li>
                         </ul>
                     </li>
                 </ul>
