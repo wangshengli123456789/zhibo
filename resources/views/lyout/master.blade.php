478cafe82454284e90f70866a175780bf89ce8eb
王胜利 <1414210968@qq.com>
2018-12-07 19:34:48 +0800

    增加了帮助中心，以及联系我们

--- resources/views/lyout/master.blade.php
+++ resources/views/lyout/master.blade.php
@@ -42,8 +42,8 @@
                         <a href="#"><i class="icon-font">&#xe018;</i>系统管理</a>
                         <ul class="sub-menu">
                             <li><a href="system.html"><i class="icon-font">&#xe017;</i>系统设置</a></li>
-                            {{--<li><a href="system.html"><i class="icon-font">&#xe037;</i>清理缓存</a></li>--}}
-                            {{--<li><a href="system.html"><i class="icon-font">&#xe046;</i>数据备份</a></li>--}}
+                            <li><a href="system.html"><i class="icon-font">&#xe037;</i>帮助中心</a></li>
+                            <li><a href="system.html"><i class="icon-font">&#xe046;</i>联系我们</a></li>
                             {{--<li><a href="system.html"><i class="icon-font">&#xe045;</i>数据还原</a></li>--}}
                         </ul>
                     </li>
